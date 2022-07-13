5c31d2e213e71649cd35ba030f605f39e671b6cd
Vikram Rajput <vikramrajput@Vikrams-Macbook-Pro.local>
2022-07-14 00:33:19 +0530

    206. Reverse Linked List

--- Easy/206-Reverse-Linked-List.js
+++ Easy/206-Reverse-Linked-List.js
@@ -4,6 +4,8 @@
  *     this.val = (val===undefined ? 0 : val)
  *     this.next = (next===undefined ? null : next)
  * }
+ * Runtime: 99 ms, faster than 42.83% of JavaScript online submissions for Reverse Linked List.
+* Memory Usage: 44.4 MB, less than 43.54% of JavaScript online submissions for Reverse Linked List.
  */
 /**
  * @param {ListNode} head
