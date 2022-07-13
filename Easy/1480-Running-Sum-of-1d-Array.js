6cf7b99849b44bdc1d47df8bac2e46a41aab2136
Vikram Rajput <2268997+vikram-rajput@users.noreply.github.com>
2022-07-14 00:59:50 +0530

    Update 1480-Running-Sum-of-1d-Array.js

--- Easy/1480-Running-Sum-of-1d-Array.js
+++ Easy/1480-Running-Sum-of-1d-Array.js
@@ -3,12 +3,28 @@
  * @return {number[]}
  */
  var runningSum = function(nums) {
-    sum = nums[0];
+    sum = 0;
     ans = [];
-    for(var i = 1; i< nums.length; i++){
+    for(var i = 0; i< nums.length; i++){
         sum += nums[i];
         ans.push(sum);
         // nums[i] += nums[i -1];
     }
     return ans;
+};
+
+
+----- or -----
+ 
+ /**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function(nums) {
+    let sum = [];
+    sum[0] = nums[0];
+    for(var i = 1; i< nums.length; i++){
+        sum[i] = nums[i] + sum[i-1];
+    }
+    return sum;
 };
