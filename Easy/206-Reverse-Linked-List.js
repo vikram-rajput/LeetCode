fddcc0a6953c94e78793476a7fb8c6d7f095f422
Vikram Rajput <vikramrajput@Vikrams-Macbook-Pro.local>
2022-07-14 00:31:28 +0530

    206. Reverse Linked List

---
+++ Easy/206-Reverse-Linked-List.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+ var reverseList = function(head) {
+    var prev = null;
+    var curr = head;
+    while (curr){
+        let nxt = curr.next;
+        curr.next = prev;
+        prev = curr
+        curr = nxt;
+    }
+    return prev;
+};
