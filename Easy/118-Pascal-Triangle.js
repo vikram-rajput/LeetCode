59279a471ad688015d29772700ff15b806d19b23
Vikram Rajput <vikramrajput1987@gmail.com>
2022-07-19 23:48:01 +0530

    118. Pascal's Triangle

---
+++ Easy/118-Pascal-Triangle.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+ var generate = function(numRows) {
+    if(numRows < 1 ){ return []}
+    if(numRows === 1) return [[1]];
+    const result = [[1]];
+    for(var i= 1; i< numRows; i++){
+        let preRow = result[i-1];
+        const curRow = [];
+        curRow.push(1);
+        for(let j = 1; j < preRow.length; j++){
+            curRow[j] = preRow[j] + preRow[j -1];
+        }
+        curRow.push(1);
+        result.push(curRow);
+    }
+    return result;
+};
