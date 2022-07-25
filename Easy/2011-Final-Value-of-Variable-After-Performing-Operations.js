212befb6ac88a7011733b62240581e5cb3c72cbd
Vikram Rajput <vikramrajput1987@gmail.com>
2022-07-25 23:40:06 +0530

    2011. Final Value of Variable After Performing Operations

---
+++ Easy/2011-Final-Value-of-Variable-After-Performing-Operations.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string[]} operations
+ * @return {number}
+ */
+ var finalValueAfterOperations = function(operations) {
+    var x =0;
+        for(var i=0;i<operations.length;i++){
+            if(operations[i]=="--X" ||operations[i]=="X--" )
+                x = x-1;
+            else if(operations[i]=="++X" ||operations[i]=="X++")
+                x = x+1;
+            else
+                continue;
+        }
+        return x;
+};
