9025a218b8de1404b4ee1bb4dd77bd0c516985a8
Vikram Rajput <vikramrajput1987@gmail.com>
2022-07-28 23:50:45 +0530

    1512. Number of Good Pairs

--- Easy/1512-Number-of-Good-Pairs.js
+++ Easy/1512-Number-of-Good-Pairs.js
@@ -3,7 +3,7 @@
  * @param {number[]} nums
  * @return {number}
  */
- var numIdenticalPairs = function(nums) {
+var numIdenticalPairs = function (nums) {
     let result = 0
     for (i = 0; i < nums.length - 1; i++) {
         for (j = i + 1; j < nums.length; j++) {
