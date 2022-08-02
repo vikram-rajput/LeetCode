64232b26bd6413ccb7a869634331ed2bf7f6084c
Vikram Rajput <vikramrajput1987@gmail.com>
2022-08-02 21:25:26 +0530

    378. Kth Smallest Element in a Sorted Matrix

--- Medium/378-Kth-Smallest-Element-in-a-Sorted-Matrix.js
+++ Medium/378-Kth-Smallest-Element-in-a-Sorted-Matrix.js
@@ -3,21 +3,21 @@
  * @param {number} k
  * @return {number}
  */
- var kthSmallest = function(matrix, k) {
+var kthSmallest = function (matrix, k) {
     let len = matrix.length;
     let left = matrix[0][0];
-    let right = matrix[len-1][len-1];
+    let right = matrix[len - 1][len - 1];
     let m;
-    while(left < right){
-        m = Math.floor((left+right)/2);
+    while (left < right) {
+        m = Math.floor((left + right) / 2);
         let lessOrEqualM = 0;
         matrix.forEach(row => {
             row.forEach(num => {
-                if(num<=m) lessOrEqualM++;
+                if (num <= m) lessOrEqualM++;
             })
         })
-        if(lessOrEqualM>=k) right = m;
-        if(lessOrEqualM<k) left = m + 1;
+        if (lessOrEqualM >= k) right = m;
+        if (lessOrEqualM < k) left = m + 1;
     }
-return left; 
+    return left;
 };
