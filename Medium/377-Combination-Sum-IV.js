3f91018d322ddca0e49cf70b6fdf166f5d9bf1a6
Vikram Rajput <vikramrajput1987@gmail.com>
2022-08-05 21:23:03 +0530

    377. Combination Sum IV

--- Medium/377-Combination-Sum-IV.js
+++ Medium/377-Combination-Sum-IV.js
@@ -3,17 +3,17 @@
  * @param {number} target
  * @return {number}
  */
- var combinationSum4 = function(nums, target) {
+var combinationSum4 = function (nums, target) {
     var memo = {}
-   function helper(target) {
-       if (target < 0) return 0;
-       if (target == 0) return 1;
-       if (target in memo) return memo[target];
-       var res = 0, n = nums.length
-       for (var i = 0; i < n; ++i) {
-           res += helper(target - nums[i]);
-       }
-       return memo[target] = res;
-   }
-   return helper(target);
+    function helper(target) {
+        if (target < 0) return 0;
+        if (target == 0) return 1;
+        if (target in memo) return memo[target];
+        var res = 0, n = nums.length
+        for (var i = 0; i < n; ++i) {
+            res += helper(target - nums[i]);
+        }
+        return memo[target] = res;
+    }
+    return helper(target);
 };
