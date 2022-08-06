25857d7608d392a9e775e06a95749fdf0041f5d2
Vikram Rajput <vikramrajput1987@gmail.com>
2022-08-06 22:28:37 +0530

    1365. How Many Numbers Are Smaller Than the Current Number

---
+++ Easy/1365-How-Many-Numbers-Are-Smaller-Than-the-Current-Number.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var smallerNumbersThanCurrent = function (nums) {
+    const sorted = [...nums].sort((a, b) => a - b);
+
+    return nums.map(num => sorted.indexOf(num));
+};
