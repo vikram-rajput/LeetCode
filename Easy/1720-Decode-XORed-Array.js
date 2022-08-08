2f36ca9ea664cf137eb14079a9505d2149158f0c
Vikram Rajput <vikramrajput1987@gmail.com>
2022-08-08 08:00:18 +0530

    1720. Decode XORed Array

--- Easy/1720-Decode-XORed-Array.js
+++ Easy/1720-Decode-XORed-Array.js
@@ -3,10 +3,10 @@
  * @param {number} first
  * @return {number[]}
  */
- var decode = function(encoded, first) {
+var decode = function (encoded, first) {
     const res = [first];
-         for(let i=0; i<encoded.length; i++){
-             res.push(encoded[i]^res[i]);
-         }
-         return res;
- }
+    for (let i = 0; i < encoded.length; i++) {
+        res.push(encoded[i] ^ res[i]);
+    }
+    return res;
+}
