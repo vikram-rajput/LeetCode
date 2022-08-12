a870d0d555ba571fd3f1090897f33a763ed31c3c
Vikram Rajput <vikramrajput1987@gmail.com>
2022-08-12 22:29:36 +0530

    1389. Create Target Array in the Given Order

---
+++ Easy/1389-Create-Target-Array-in-the-Given-Order.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number[]} index
+ * @return {number[]}
+ */
+ var createTargetArray = function(nums, index) {
+    const N = nums.length
+  let res = Array(N).fill(null)
+  for (let i = 0; i < N; i++) {
+    if (res[index[i]] == null) {
+      res[index[i]] = nums[i]
+    } else {
+      res = [res.slice(0, index[i]), nums[i], res.slice(index[i])].flat()
+    }
+  }
+  return res.filter(x => x !== null)
+};
