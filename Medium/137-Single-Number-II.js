0e9a74cdf4f97dd2486b7dc997cdfcbd840f22ed
Vikram Rajput <vikramrajput1987@gmail.com>
2022-08-21 13:49:59 +0530

    137. Single Number II

---
+++ Medium/137-Single-Number-II.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var singleNumber = function(nums) {
+    let ones = 0;
+    let twos = 0;
+    
+    for(num of nums){
+        ones = ( ones ^ num ) & (~twos);
+        twos = ( twos ^ num ) & (~ones);
+    }
+    return ones;
+};
