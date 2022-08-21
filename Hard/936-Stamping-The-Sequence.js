7003cbaaed867396065af4d12c3649a3ad1b8747
Vikram Rajput <vikramrajput1987@gmail.com>
2022-08-21 11:16:31 +0530

    936. Stamping The Sequence

--- Hard/936-Stamping-The-Sequence.js
+++ Hard/936-Stamping-The-Sequence.js
@@ -3,16 +3,16 @@
  * @param {string} target
  * @return {number[]}
  */
- var movesToStamp = function(stamp, target) {
+var movesToStamp = function (stamp, target) {
     if (stamp === target) return [0]
     let slen = stamp.length, tlen = target.length - slen + 1,
         ans = [], tdiff = true, sdiff, i, j,
-    S = stamp.split(""), T = target.split("")
+        S = stamp.split(""), T = target.split("")
     while (tdiff)
         for (i = 0, tdiff = false; i < tlen; i++) {
             for (j = 0, sdiff = false; j < slen; j++)
-                if (T[i+j] === "*") continue
-                else if (T[i+j] !== S[j]) break
+                if (T[i + j] === "*") continue
+                else if (T[i + j] !== S[j]) break
                 else sdiff = true
             if (j === slen && sdiff) {
                 for (j = i, tdiff = true; j < slen + i; j++)
