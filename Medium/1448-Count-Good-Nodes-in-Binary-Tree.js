27a4d77d561420b25a6d37f5266b951bf9e3af4e
Vikram Rajput <vikramrajput1987@gmail.com>
2022-09-01 05:41:41 +0530

    1448. Count Good Nodes in Binary Tree

--- Medium/1448-Count-Good-Nodes-in-Binary-Tree.js
+++ Medium/1448-Count-Good-Nodes-in-Binary-Tree.js
@@ -10,13 +10,13 @@
  * @param {TreeNode} root
  * @return {number}
  */
- var goodNodes = function(root) {
+var goodNodes = function (root) {
     let stack = [[root, root.val]], count = 0;
     while (stack.length) {
-      let [node, currMax] = stack.pop();
-      if (node.val >= currMax) count++;
-      if (node.left) stack.push([node.left, Math.max(currMax, node.left.val)]);
-      if (node.right) stack.push([node.right, Math.max(currMax, node.right.val)]);  
+        let [node, currMax] = stack.pop();
+        if (node.val >= currMax) count++;
+        if (node.left) stack.push([node.left, Math.max(currMax, node.left.val)]);
+        if (node.right) stack.push([node.right, Math.max(currMax, node.right.val)]);
     }
     return count;
-  };
+};
