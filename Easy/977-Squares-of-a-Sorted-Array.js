e040d76c6ac8029f12031a50343af586ff970a03
Vikram Rajput <vikramrajput1987@gmail.com>
2022-09-01 06:22:54 +0530

    977. Squares of a Sorted Array

--- Easy/977-Squares-of-a-Sorted-Array.js
+++ Easy/977-Squares-of-a-Sorted-Array.js
@@ -2,11 +2,11 @@
  * @param {number[]} nums
  * @return {number[]}
  */
- var sortedSquares = function(nums) {
+var sortedSquares = function (nums) {
     nums
-  var n = [];
-   for(let i in nums){
-   n.push(nums[i]*nums[i])
-  } 
-  return n.sort((a,b)=>a-b);
+    var n = [];
+    for (let i in nums) {
+        n.push(nums[i] * nums[i])
+    }
+    return n.sort((a, b) => a - b);
 };
