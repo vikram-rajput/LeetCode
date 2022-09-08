2c5a277264cf3ecff62051e34c466d7703756606
Vikram Rajput <vikramrajput1987@gmail.com>
2022-09-08 10:51:22 +0530

    94. Binary Tree Inorder Traversal

---
+++ Easy/94-Binary-Tree-Inorder-Traversal.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+ var inorderTraversal = function(root) {
+    let res = [];
+   dfs(root);
+   return res;
+ 
+   function dfs(node) {
+     if (!node) return;
+     dfs(node.left);
+     res.push(node.val);
+     dfs(node.right);
+   }   
+ };
