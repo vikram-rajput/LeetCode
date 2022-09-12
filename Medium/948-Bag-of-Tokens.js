aa4ddabec742c73f13f46c9f6a6c742d310eca38
Vikram Rajput <vikramrajput1987@gmail.com>
2022-09-12 09:49:19 +0530

    948. Bag of Tokens

--- Medium/948-Bag-of-Tokens.js
+++ Medium/948-Bag-of-Tokens.js
@@ -38,3 +38,40 @@ const merge = (left, right) => {
     }
     return result.concat(left.length ? left : right);
 }
+
+
+
+
+// solution 2
+let points = 0; // our rolling score
+let result = 0; // to record highest score we've seen
+let i = 0; // counter in the front, for spending power and gaining points
+let j = tokens.length - 1; // counter in the back, for gaining power
+// sort so we get the cheapest tokens up front
+tokens.sort((a, b) => a - b);
+while (i <= j) {
+    // look at a token at the start, can we use it to gain score?
+    if (P >= tokens[i]) {
+        // if so, lower your power and raise your score
+        P = P - tokens[i];
+        points = points + 1;
+        // Now record the best result we've seen
+        // This covers the scenario where we later spend a point to gain power
+        // but we we're never able to capitalize on it by turning that power
+        // into more scoring points
+        result = Math.max(result, points);
+        // next round we'll consider the next token to spend power on
+        i++;
+    } else if (points > 0) {
+        // At this time we can't gain score with a token in front, so let's
+        // spend a point of our score to gain more power
+        points = points - 1;
+        P = P + tokens[j];
+        // next round we'll consider the next highest power to gain
+        j--;
+    } else {
+        // if we can't do either, stop
+        break;
+    }
+}
+return result;
