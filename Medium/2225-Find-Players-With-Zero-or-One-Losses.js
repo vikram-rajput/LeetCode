e6eda030aea6e94510fc369181b45b73817a1169
Vikram Rajput <vikramrajput1987@gmail.com>
2022-11-28 23:03:18 +0530

    2225. Find Players With Zero or One Losses

---
+++ Medium/2225-Find-Players-With-Zero-or-One-Losses.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[][]} matches
+ * @return {number[][]}
+ */
+ var findWinners = function(matches) {
+    let lost = {};
+  for (let [winner, loser] of matches) {
+    lost[winner] = (lost[winner] || 0);
+    lost[loser] = (lost[loser] || 0) + 1;
+  }
+  let res = [[], []];
+  for (let player in lost) {
+    if (lost[player] === 0) res[0].push(+player);
+    else if (lost[player] === 1) res[1].push(+player);
+  }
+  return res;
+};
